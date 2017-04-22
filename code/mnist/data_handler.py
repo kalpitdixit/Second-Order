f0c52ae3853e287645726df1e18867f0cb7f070f
Kalpit Dixit <kalpit@stanford.edu>
2017-04-22 13:38:00 -0700

    now reads validation data

--- code/mnist/data_handler.py
+++ code/mnist/data_handler.py
@@ -6,6 +6,7 @@ class Dataset(object):
         self.data_dir = data_dir
         self.data = self.get_data()
         self.n_train = self.data['train_images'].shape[0]
+        self.n_val   = self.data['val_images'].shape[0]
         self.n_test  = self.data['test_images'].shape[0]
 
     def get_data(self):
@@ -13,6 +14,8 @@ class Dataset(object):
         "data" - dict
         data['train_images'] - 2d np array. data['train_images'][i] refers to train image number i. 2nd dim is 784.
         data['train_labels'] - 1d list. data['train_labels'][i] refers to label of train image number i.
+        data['val_images'] - 2d np array. data['val_images'][i] refers to val image number i. 2nd dim is 784.
+        data['val_labels'] - 1d list. data['val_labels'][i] refers to label of val image number i.
         data['test_images'] - 2d np array. data['test_images'][i] refers to test image number i. 2nd dim is 784.
         data['test_labels'] - 1d list. data['test_labels'][i] refers to label of test image number i.
         """
@@ -21,6 +24,9 @@ class Dataset(object):
         ### training
         data['train_images'] = np.load(os.path.join(self.data_dir, 'training', 'images.npy'))
         data['train_labels'] = np.load(os.path.join(self.data_dir, 'training', 'labels.npy'))
+        ### validation
+        data['val_images'] = np.load(os.path.join(self.data_dir, 'validation', 'images.npy'))
+        data['val_labels'] = np.load(os.path.join(self.data_dir, 'validation', 'labels.npy'))
         ### testing
         data['test_images'] = np.load(os.path.join(self.data_dir, 'testing', 'images.npy'))
         data['test_labels'] = np.load(os.path.join(self.data_dir, 'testing', 'labels.npy'))
